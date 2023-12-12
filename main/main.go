4d0c6a14d259615c85196aea92aa78d2b29d8eec
Anna Verkhogliadova <verh2511@icloud.com>
2023-12-12 12:16:32 +0300

    24

--- main/main.go
+++ main/main.go
@@ -4,21 +4,4 @@ func main() {
 	println("kfjglkdfgkjhdf")
 	println("fgf6")
 	println("7485436583")
-
-	println("kfjglkdfgkjhdf")
-	println("fgf6")
-	println("7485436583")
-
-	println("567")
-	println("123")
-	println("ccc")
-	println("ddd")
-
-	println("kfjglkdfgkjhdf")
-	println("fgf6")
-	println("7485436583")
-
-	println("kfjglkdfgkjhdf")
-	println("fgf6")
-	println("7485436583")
 }
