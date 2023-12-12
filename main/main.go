7cc96620b492d292db50a714353d0133bcb0f661
Anna Verkhogliadova <verh2511@icloud.com>
2023-12-12 12:26:33 +0300

    26

--- main/main.go
+++ main/main.go
@@ -18,4 +18,14 @@ func main() {
 	println("123")
 	println("ccc")
 	println("ddd")
+
+	println("567")
+	println("123")
+	println("ccc")
+	println("ddd")
+
+	println("567")
+	println("123")
+	println("ccc")
+	println("ddd")
 }
