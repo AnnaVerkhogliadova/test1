f1a14c093a8a49a9a1fae0688cc8bed502d20aa4
Anna Verkhogliadova <verh2511@icloud.com>
2023-12-12 10:56:24 +0300

    15

--- main/main.go
+++ main/main.go
@@ -3,10 +3,13 @@ package main
 func main() {
 	println("123")
 	println("123")
-	println("ddd")
-	println("hhh")
-	println("jjj")
-	println("anna")
-	println("anna")
-	println("HELLO")
+
+	println("123")
+	println("123")
+
+	println("123")
+	println("123")
+
+	println("123")
+	println("123")
 }
